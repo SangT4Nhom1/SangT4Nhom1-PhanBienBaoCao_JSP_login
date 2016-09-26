fc5d945fa12948515acdf18eb5a91c1c8fb90269
NguyenThang <NguyenThang@172.16.46.108>
2016-09-26 10:53:37 +0700

    sau lan 2 2 2

--- EX1/src/package1/thang.java
+++ EX1/src/package1/thang.java
@@ -3,6 +3,9 @@ package package1;
 public class thang {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		
 	}
 
 }
