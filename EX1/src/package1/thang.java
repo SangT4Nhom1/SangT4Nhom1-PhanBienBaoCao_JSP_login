396ed5595a4afcd06b53a3af236e84a12e4f6cf6
NguyenThang <NguyenThang@172.16.46.108>
2016-09-26 11:03:07 +0700

    sau lan 4

--- EX1/src/package1/thang.java
+++ EX1/src/package1/thang.java
@@ -7,6 +7,7 @@ public class thang {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 		
 		
 	}
